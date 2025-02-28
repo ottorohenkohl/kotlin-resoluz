545cb1ea56402e1893c055df7cf5673e3d931252
Otto Rohenkohl <message@rohenkohl.dev>
2025-02-28 15:09:42 +0100

    fix: allowing elimination of unequal sized sets

--- src/main/kotlin/domain/service/ResolutionsService.kt
+++ src/main/kotlin/domain/service/ResolutionsService.kt
@@ -4,16 +4,6 @@ import dev.rohenkohl.domain.model.Literal
 
 class ResolutionsService {
 
-    private fun isEliminierbar(aktuell: Set<Literal>, naechstes: Set<Literal>): Boolean {
-        val schnittmenge = aktuell.intersect(naechstes)
-        val sdifferenz = aktuell.union(naechstes).minus(schnittmenge)
-
-        if (aktuell.size != naechstes.size) return false
-        if (aktuell.size - 1 != schnittmenge.size) return false
-
-        return sdifferenz.toList()[0].isUmkehr(sdifferenz.toList()[1])
-    }
-
     fun isAntinomie(klauseln: Set<Set<Literal>>): Boolean {
         val geordnet = klauseln.toList()
 
@@ -23,14 +13,17 @@ class ResolutionsService {
         val aktuelle = mutableListOf(geordnet[0])
 
         while (true) {
-            if (isEliminierbar(aktuelle.last(), geordnet[indices.last()])) {
-                aktuelle.addLast(aktuelle.last().intersect(geordnet[indices.last()]))
+            val vereinigung = aktuelle.last().union(geordnet[indices.last()])
+            val eliminierbar = vereinigung.filter { vereinigung.any(it::isUmkehr) }.toSet()
+
+            if (eliminierbar.size == 2) {
+                aktuelle.addLast(vereinigung.minus(eliminierbar))
                 indices.addLast(0)
 
                 if (aktuelle.last().isEmpty()) return true else continue
             }
 
-            if (indices.last() == geordnet.lastIndex) {
+            while (indices.last() == geordnet.lastIndex) {
                 if (indices.size == 1) return false
 
                 indices.removeLast()
